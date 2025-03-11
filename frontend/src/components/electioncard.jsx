dc1fb73d729c481e20a6683c39d1c5938a355805
Dhanush Sarva <sarvadhanush469@gmail.com>
2025-03-11 07:56:22 +0530

    added better way to show no elections in website

--- frontend/src/components/electioncard.jsx
+++ frontend/src/components/electioncard.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 function ElectionCard() {
@@ -15,40 +14,50 @@ function ElectionCard() {
         setElections(data);
       });
   }, []);
-  
 
   return (
     <>
-    <div className="bg-black w-full min-h-screen gap-4 flex-wrap flex justify-center items-center">
-      {elections.map((election, index) => (
-        <div key={index} className="w-45 p-2 bg-red-600 rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
-          <img
-            className="h-40 object-cover rounded-xl"
-            src="../../../public/logos/electioncard.jpg"
-            alt=""
-          />
-          <div className="p-2">
-            <h2 className="font-bold text-white text-lg mb-2">{election.active}</h2>
-            <p className="text-lg font-bold text-white">{election.electionName}</p>
-          </div>
-          <div className="m-2">
-            {election.active ? (
-              <Link
-                role="button"
-                className="text-red-600 bg-white px-3 py-1 rounded-md hover:bg-red-500 hover:text-white"
-                to={`/electionDetails/${election.election_id}`}
-              >
-                Vote Now
-              </Link>
-            ) : (
-              <span className="text-gray-400 bg-gray-700 px-3 py-1 rounded-md cursor-not-allowed">
-                Election Closed
-              </span>
-            )}
-          </div>
-        </div>
-      ))}
-    </div>
+      <div className="bg-black w-full min-h-screen gap-4 flex-wrap flex justify-center items-center">
+        {elections.length === 0 ? (
+          <div className='flex flex-col justify-center items-center h-screen'>
+              <h2 className='text-center text-5xl font-extrabold text-red-500 '>No Elections</h2>
+              <blockquote className='text-center text-gray-200 mt-30'>Come Back Later!  Let's go back </blockquote>
+              <Link to="/" ><button className='bg-red-600 w-[180px] rounded-md font-semibold py-2 my-8'>Take Me Home</button></Link>
+            </div>
+        ) : (
+          elections.map((election, index) => (
+            <div
+              key={index}
+              className="w-45 p-2 bg-red-600 rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl"
+            >
+              <img
+                className="h-40 object-cover rounded-xl"
+                src="../../../public/logos/electioncard.jpg"
+                alt=""
+              />
+              <div className="p-2">
+                <h2 className="font-bold text-white text-lg mb-2">{election.active}</h2>
+                <p className="text-lg font-bold text-white">{election.electionName}</p>
+              </div>
+              <div className="m-2">
+                {election.active ? (
+                  <Link
+                    role="button"
+                    className="text-red-600 bg-white px-3 py-1 rounded-md hover:bg-red-500 hover:text-white"
+                    to={`/electionDetails/${election.election_id}`}
+                  >
+                    Vote Now
+                  </Link>
+                ) : (
+                  <span className="text-gray-400 bg-gray-700 px-3 py-1 rounded-md cursor-not-allowed">
+                    Election Closed
+                  </span>
+                )}
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </>
   );
 }
